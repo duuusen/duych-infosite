77094182a5f5dc487031c00fae2a0ebdb7c2f5d9
duuusen <halloduy@gmail.com>
2019-04-28 19:59:45 +0200

    fancy letter animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,28 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`Duy Bui`, `Interaction Design`]} />
     <h1>Duy Bui</h1>
-    <p className={'shake-crazy iad'}>Interaction Designer</p>
+    <p className={'shake-crazy iad'}>
+      <span className={'i'}>I</span>
+      <span className={'n'}>n</span>
+      <span className={'t'}>t</span>
+      <span className={'e'}>e</span>
+      <span className={'r'}>r</span>
+      <span className={'a'}>a</span>
+      <span className={'c'}>c</span>
+      <span className={'t2'}>t</span>
+      <span className={'i2'}>i</span>
+      <span className={'o'}>o</span>
+      <span className={'n2'}>n</span>
+      <span> </span>
+      <span className={'d'}>D</span>
+      <span className={'e2'}>e</span>
+      <span className={'s'}>s</span>
+      <span className={'i3'}>i</span>
+      <span className={'g'}>g</span>
+      <span className={'n3'}>n</span>
+      <span className={'e3'}>e</span>
+      <span className={'r2'}>r</span>
+    </p>
     <br/>
     <a href="mailto:hello@duy.ch">hello@duy.ch</a>
     <ul class="links">
